de447da1df96ea40ff07ade391d9552f821b60fd
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-14 17:42:52 -0400

    Implement caching proxy server with command-line interface

---
+++ bin/caching_proxy.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(File.expand_path("../lib", __dir__))
+
+require 'rack'
+require 'caching_proxy/cli'
+require 'caching_proxy/server'
+require 'caching_proxy/cache'
+
+options = CachingProxy::Cli.parse_args
+cache = CatchingPorxy::Cache.new
+
+if options[:clear_cache]
+  cache.clear
+  puts "Cache cleared"
+  exit
+end
+
+if options[:port] && options[:origin]
+  begin
+    app = CachiongProxy::Server.new(options[:origin], cache)
+    Rack::Handler::WEBrick.run app, Port: options[:port]
+  rescue => e
+    puts "Cached server error: #{e}"
+  end
+else
+  puts "Missing --port or --origin"
+end

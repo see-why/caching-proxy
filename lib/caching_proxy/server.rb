2836d1927c9b48822dd891272c65863d2b6c2801
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-14 17:43:09 -0400

    Fix typo in response header processing and improve caching logic in Server class

---
+++ lib/caching_proxy/server.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'uri'
+
+module CachingProxy
+  class Server
+    def initialize(origin, cache)
+      @origin = origin
+      @cache = cache
+    end
+
+    def call
+      key = @origin
+
+      if @cache.key? key
+        cached = @cache[key]
+        return [cached[:status], cached[:headers].merge('X-Cache' => 'HIT'), [cached[:body]]]
+      end
+
+      uri = URI.new(@origin)
+      response = Net::HTTP.get_response(uri)
+
+      body = response.body
+      headers = {}
+      resposne.each_header { |k, v| headers[k] = v }
+
+      @cache.set(key, { status: response.code.to_i, headers: headers, body: body })
+
+      [response.code.to_i, headers.merge('X-Cache' => 'MISS'), [body]]
+    end
+  end
+end

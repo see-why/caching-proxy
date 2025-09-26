91655972a11a4e12036132a578027acf31f26d86
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-09-25 23:41:22 -0400

    Refactor Cache class to improve initialization and method implementations

--- lib/caching_proxy/cache.rb
+++ lib/caching_proxy/cache.rb
@@ -1,25 +1,47 @@
 # frozen_string_literal: true
 
+
 module CachingProxy
   class Cache
-    def initialize
+    DEFAULT_TTL = 300 # seconds
+
+    def initialize(default_ttl = DEFAULT_TTL)
       @store = {}
+      @default_ttl = default_ttl
     end
 
     def key?(key)
-      @store.key? key
+      entry = @store[key]
+      entry && !expired?(entry)
     end
 
     def get(key)
-      @store[key]
+      entry = @store[key]
+      return nil unless entry
+      return nil if expired?(entry)
+      entry[:value]
     end
 
-    def set(key, value)
-      @store[key] = value
+    def set(key, value, ttl = nil)
+      ttl ||= @default_ttl
+      @store[key] = {
+        value: value,
+        expires_at: Time.now + ttl
+      }
+    end
+
+    def invalidate(key)
+      @store.delete(key)
     end
 
     def clear
       @store.clear
+    end
+
+    private
+
+    def expired?(entry)
+      Time.now > entry[:expires_at]
     end
   end
 end

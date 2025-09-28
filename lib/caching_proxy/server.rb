7648943e35ae8c9a5d6b2e9e380d7547c808c633
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-09-28 10:30:00 -0400

    Refactor caching logic to improve Cache-Control handling and streamline response flow

--- lib/caching_proxy/server.rb
+++ lib/caching_proxy/server.rb
@@ -20,13 +20,26 @@ module CachingProxy
       url = "#{@origin}#{path_info}"
       url += "?#{query_string}" unless query_string.nil? || query_string.empty?
 
-      if @cache.key? url
-        cached = @cache.get(url)
+      cached = @cache.get(url)
+      # If cached and not marked no-cache, serve from cache
+      if cached && !(cached[:headers]['cache-control'].to_s.downcase.include?('no-cache'))
         return [cached[:status], cached[:headers].merge('X-Cache' => 'HIT'), [cached[:body]]]
       end
 
+      # If cached and marked no-cache, revalidate with origin
       uri = URI.parse(url)
-      response = Net::HTTP.get_response(uri)
+      req = Net::HTTP::Get.new(uri)
+      if cached && cached[:headers]
+        if cached[:headers]['etag']
+          req['If-None-Match'] = cached[:headers]['etag']
+        end
+        if cached[:headers]['last-modified']
+          req['If-Modified-Since'] = cached[:headers]['last-modified']
+        end
+      end
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == 'https')
+      response = http.request(req)
 
       body = response.body
       headers = {}
@@ -34,25 +47,22 @@ module CachingProxy
 
       # Handle Cache-Control headers
       cache_control = headers['cache-control'] || headers['Cache-Control']
-      cacheable = true
+      directives = cache_control.to_s.downcase.split(',').map(&:strip)
+      max_age = directives.find { |d| d.start_with?('max-age=') }
       ttl = nil
-      if cache_control
-        directives = cache_control.downcase.split(',').map(&:strip)
-        if directives.include?('no-cache') || directives.include?('must-revalidate')
-          cacheable = false
-        end
-        max_age = directives.find { |d| d.start_with?('max-age=') }
-        if max_age
-          ttl_val = max_age.split('=', 2)[1]
-          ttl = ttl_val.to_i if ttl_val =~ /^\d+$/
-        end
+      ttl = max_age.split('=', 2)[1].to_i if max_age && max_age.split('=', 2)[1] =~ /^\d+$/
+
+      # If 304 Not Modified, serve cached response
+      if response.code.to_i == 304 && cached
+        return [cached[:status], cached[:headers].merge('X-Cache' => 'REVALIDATED'), [cached[:body]]]
       end
 
-      if cacheable
-        @cache.set(url, { status: response.code.to_i, headers: headers, body: body }, ttl)
-      end
+      # Store in cache (even if no-cache, but always revalidate before serving)
+      @cache.set(url, { status: response.code.to_i, headers: headers, body: body }, ttl)
 
-      [response.code.to_i, headers.merge('X-Cache' => cacheable ? 'MISS' : 'BYPASS'), [body]]
+      # If no-cache, mark as BYPASS, else MISS
+      x_cache = directives.include?('no-cache') ? 'BYPASS' : 'MISS'
+      [response.code.to_i, headers.merge('X-Cache' => x_cache), [body]]
     end
   end
 end

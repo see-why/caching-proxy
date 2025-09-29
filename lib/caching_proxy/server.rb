f7060de86a3d132bf6dae47c215f7d36ce004e8a
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-09-29 00:15:43 -0400

    Refactor caching logic to improve Cache-Control handling and streamline response flow

--- lib/caching_proxy/server.rb
+++ lib/caching_proxy/server.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'uri'
 
+
 module CachingProxy
   class Server
     def initialize(origin, cache)
@@ -21,36 +22,27 @@ module CachingProxy
       url += "?#{query_string}" unless query_string.nil? || query_string.empty?
 
       cached = @cache.get(url)
+      cache_control_directives = cached ? parse_cache_control(cached[:headers]) : []
+
       # If cached and not marked no-cache, serve from cache
-      if cached && !(cached[:headers]['cache-control'].to_s.downcase.include?('no-cache'))
-        return [cached[:status], cached[:headers].merge('X-Cache' => 'HIT'), [cached[:body]]]
+      unless cache_control_directives.include?('no-cache')
+        if cached
+          return [cached[:status], cached[:headers].merge('X-Cache' => 'HIT'), [cached[:body]]]
+        end
       end
 
       # If cached and marked no-cache, revalidate with origin
       uri = URI.parse(url)
       req = Net::HTTP::Get.new(uri)
-      if cached && cached[:headers]
-        if cached[:headers]['etag']
-          req['If-None-Match'] = cached[:headers]['etag']
-        end
-        if cached[:headers]['last-modified']
-          req['If-Modified-Since'] = cached[:headers]['last-modified']
-        end
-      end
+      add_conditional_headers(req, cached)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = (uri.scheme == 'https')
       response = http.request(req)
 
       body = response.body
-      headers = {}
-      response.each_header { |k, v| headers[k] = v if k.to_s.downcase != 'transfer-encoding' }
-
-      # Handle Cache-Control headers
-      cache_control = headers['cache-control'] || headers['Cache-Control']
-      directives = cache_control.to_s.downcase.split(',').map(&:strip)
-      max_age = directives.find { |d| d.start_with?('max-age=') }
-      ttl = nil
-      ttl = max_age.split('=', 2)[1].to_i if max_age && max_age.split('=', 2)[1] =~ /^\d+$/
+      headers = extract_headers(response)
+      directives = parse_cache_control(headers)
+      ttl = extract_ttl(directives)
 
       # If 304 Not Modified, serve cached response
       if response.code.to_i == 304 && cached
@@ -68,6 +60,36 @@ module CachingProxy
       # If no-cache, mark as BYPASS, else MISS
       x_cache = directives.include?('no-cache') ? 'BYPASS' : 'MISS'
       [response.code.to_i, headers.merge('X-Cache' => x_cache), [body]]
+    end
+
+    private
+
+    def parse_cache_control(headers)
+      cc = headers['cache-control'] || headers['Cache-Control']
+      cc.to_s.downcase.split(',').map(&:strip)
+    end
+
+    def extract_ttl(directives)
+      max_age = directives.find { |d| d.start_with?('max-age=') }
+      return nil unless max_age
+      val = max_age.split('=', 2)[1]
+      val =~ /^\d+$/ ? val.to_i : nil
+    end
+
+    def add_conditional_headers(req, cached)
+      return unless cached && cached[:headers]
+      if cached[:headers]['etag']
+        req['If-None-Match'] = cached[:headers]['etag']
+      end
+      if cached[:headers]['last-modified']
+        req['If-Modified-Since'] = cached[:headers]['last-modified']
+      end
+    end
+
+    def extract_headers(response)
+      headers = {}
+      response.each_header { |k, v| headers[k] = v if k.to_s.downcase != 'transfer-encoding' }
+      headers
     end
   end
 end
